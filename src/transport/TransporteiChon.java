340805bb45b3337e53f0eac0049e065afb3e0e5e
frajavimopu <frajavimopu@gmail.com>
2023-10-25 17:23:43 -0500

    Add method sfbNE

--- src/transport/TransporteiChon.java
+++ src/transport/TransporteiChon.java
@@ -12,10 +12,17 @@ public class TransporteiChon {
 
     private int originPoint;
     private int destinyPoint;
+    private float originAmount[];
+    private float destinyAmount[];
     private float cost[][];
     private float var[][];
     private int newVBRow;
     private int newVBCol;
+    
+    // for sbf
+    private boolean I;
+    private boolean J;
+    private float   val;
 
     public TransporteiChon(int originPoint, int destinyPoint) {
         this.originPoint = originPoint;
@@ -31,17 +38,65 @@ public class TransporteiChon {
     }
 
     public void setOriginAmount(float amount[]) {
+        this.originAmount = amount;
         for (int i = 0; i < amount.length; i++) {
             var[i][destinyPoint] = amount[i];
         }
     }
 
     public void setDestinyAmount(float amount[]) {
+        this.destinyAmount = amount;
         for (int i = 0; i < amount.length; i++) {
             var[originPoint][i] = amount[i];
         }
     }
 
+    private void myMin(int i, int j) {
+        I = false;
+        J = false;
+        if (var[destinyPoint][j] < var[i][originPoint]) {
+            J = true;
+            val =  var[destinyPoint][j];
+        } else {
+            I = true;
+            val =  var[i][originPoint];
+        }
+    }
+    /*
+    i = 0, j = 0
+    val = 0.0f, I =false, J = false
+    whuile( i != m-1 && j != n-1) {
+        min (i , j)  => changes I o J and val
+        var[i][J] = val;
+        s_i -= val, d_i = -= val
+        if (I) i++
+        if (J) j++
+        I = false
+        J = false
+    }
+    */
+    public void sfbNE() {
+        int i = 0, j = 0;
+        while( i != originPoint && j != destinyPoint ){ 
+            myMin(i,j);
+            var[destinyPoint][j] -= val;
+            var[i][originPoint]  -= val;
+            var[i][j] = val;
+            if (I) i++;
+            else j++;
+            System.out.println("in sfbNE----------");
+            print();
+            System.out.println("out sfbNE----------");
+        }
+        // set var[][] origin and destiny values
+        for (i = 0; i < originAmount.length; i++) {
+            var[i][destinyPoint] = originAmount[i];
+        }
+        for (j = 0; j < destinyAmount.length; j++) {
+            var[originPoint][j] = destinyAmount[j];
+        }
+    }
+    
     public void print() {
         //Imprimir la tabla de variables
         System.out.println("Tabla de variables");
@@ -58,7 +113,5 @@ public class TransporteiChon {
             }
             System.out.println("");
         }
-
     }
-
 }
--- src/transport/TransporteiChonTest.java
+++ src/transport/TransporteiChonTest.java
@@ -22,5 +22,8 @@ public class TransporteiChonTest {
        tc.setDestinyAmount(destiny);
        
        tc.print();
+       
+       tc.sfbNE();
+       tc.print();
     }
 }
